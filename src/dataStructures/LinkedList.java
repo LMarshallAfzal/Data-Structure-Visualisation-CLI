46d3fd9f4c7e0ccebd54a1ed2f5a7b66f4ecdfca
Leonard Marshall Afzal <loma256@gmail.com>
2024-02-02 12:04:26 +0000

    Add javadoc to LinkedList class

--- src/dataStructures/LinkedList.java
+++ src/dataStructures/LinkedList.java
@@ -1,8 +1,18 @@
 package dataStructures;
 
+/**
+ * Implement of a linked list data structure.
+ *
+ * <p>A linked list is a linear data structure where elements are stored in nodes, and each node
+ * points to the next one. This implementation provides methods for adding, removing, traversing,
+ * and obtaining the visual representation of the linked list.</p>
+ */
 public class LinkedList extends DataStructure {
     private Node head;
 
+    /**
+     * Inner class representing a node in the linked list.
+     */
     private static class Node {
         int value;
         Node next;
@@ -13,6 +23,16 @@ public class LinkedList extends DataStructure {
         }
     }
 
+    /**
+     * Adds a new node with the specified value to the end of the linked list.
+     *
+     * <p>This method creates a new node with the given value and appends it to the end
+     * of the linked list. If the linked list is empty, the new node becomes the head.
+     * the value of the added node is printed to the console, indicating the successful
+     * addition of the node.</p>
+     *
+     * @param value The value to be added to the linked list.
+     */
     @Override
     public void addNode(int value) {
         Node newNode = new Node(value);
@@ -29,6 +49,17 @@ public class LinkedList extends DataStructure {
         System.out.println("Added node with value: " + value);
     }
 
+    /**
+     * Removes a node with the specified value from the linked list.
+     *
+     * <p>If the linked list is empty. a message is printed indicating that the linked list
+     * is empty and the operation cannot be performed. if the node with the specified value
+     * is found, it is removed from the linked list. If the node is the head, the head is
+     * updated o the next node. If the node is not found, a message is printed indicating
+     * that the node with the specified value was not found in the linked list.</p>
+     *
+     * @param value The value of the node to be removed from the linked list.
+     */
     @Override
     public void removeNode(int value) {
         if (head == null) {
@@ -55,6 +86,12 @@ public class LinkedList extends DataStructure {
         }
     }
 
+    /**
+     * Displays the elements in the linked list.
+     *
+     * <p>This method traverses the linked list and prints each element's value to the console.
+     * If the linked list is empty, it prints a message indicating that the linked list is empty.</p>
+     */
     @Override
     public void traverse() {
         Node current = head;
@@ -66,6 +103,15 @@ public class LinkedList extends DataStructure {
         System.out.println();
     }
 
+    /**
+     * Gets the visual representation of the linked list.
+     *
+     * <p>This method creates a visual representation of the linked list, displaying each element
+     * enclosed in square brackets and connected by arrows. The representation ends with "null" to
+     * indicate the end of the linked list.</p>
+     *
+     * @return The visual representation of the linked list.
+     */
     @Override
     public String getVisualRepresentation() {
         StringBuilder visualRepresentation = new StringBuilder();
